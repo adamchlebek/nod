6a2ba3e54e16cedc9f1831e334f9f7b42f5dcb72
Adam <adam@merch.co>
2022-11-08 20:00:58 -0700

    Initial Commit

---
+++ nod/nodApp.swift
@@ -0,0 +1,17 @@
+//
+//  nodApp.swift
+//  nod
+//
+//  Created by Adam Chlebek on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct nodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
